59c748059697216234b831d589ca58e4115b6b55
AlexCarbajoG <carbajoalexander61@gmail.com>
2024-06-13 20:42:37 -0500

    insertar el archivo nuevo

---
+++ src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Category.java
@@ -0,0 +1,10 @@
+package pe.edu.idat.app_ventas_idat.model.bd;
+
+import jakarta.persistence.*;
+import org.hibernate.service.spi.InjectService;
+
+@Entity
+@Table(name="categories")
+public class Category {
+
+}
